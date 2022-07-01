6a2520782af366ba8abac023413f09170ea7ff38
Maxwell Alvord <maxwellalvord@gmail.com>
2022-07-01 08:54:29 -0700

    WIP: builing API Logic

---
+++ src/js/money.math.js
@@ -0,0 +1,14 @@
+export default class usdAgainstOthers {  
+  static async getCurrency(currency) {
+    try {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currency}`);
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      console.log(response);
+      return response.json();
+    } catch(error) {
+      return error.message;
+    }
+  }
+}
