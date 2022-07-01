7396317cb8599615c5cec67081ece74a84b7aaac
Maxwell Alvord <maxwellalvord@gmail.com>
2022-07-01 10:40:26 -0700

    WIP: API logic-UI interface logic

--- src/index.js
+++ src/index.js
@@ -5,31 +5,27 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import usdAgainstOthers from './js/money.math.js';
 
 
-
 function clearFields() {
   $('#dollar').val("");
   $('#currency').val("");
   $('#showExchange').text("");
-  $('#showErrors').text("");
+  $('#showError').text("");
 }
-
 
 function getElements(response, usd, newMoney) {
   if (response.conversion_rates) {
     const convert = (usd / response.conversion_rates.USD).toFixed(4);
     $('#showExchange').text(`With ${usd} American dollars you get ${convert} ${newMoney}s`);
   } else {
-    $('.showErrors').text(`There was an error: ${response}`);
+    $('.showError').text(`There was an error: ${response}`);
+    // $('.showErrors').text(`There was an error: ${response["error-type"]}`);
   }
 }
-
 
 async function makeApiCall(newMoney, usd) {
   const response = await usdAgainstOthers.getCurrency(newMoney);
   getElements(response, usd, newMoney);
 }
-
-
 
 $(document).ready(function() {
   $('#formOne').submit(function() {
